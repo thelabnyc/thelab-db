375f2918ed68595d96bdd2085d98959038cfdce4
Craig Weber <crgwbr@gmail.com>
2025-03-21 11:14:10 -0400

    fix: enable type checking on tests and migrations

--- thelabdb/tests/testsuniv/test_pydantic_field.py
+++ thelabdb/tests/testsuniv/test_pydantic_field.py
@@ -1,5 +1,6 @@
 from datetime import UTC, datetime
 from decimal import Decimal
+from typing import cast
 from unittest.mock import Mock
 from uuid import UUID
 
@@ -8,11 +9,13 @@ from django.db import connection
 from django.db.models.fields.json import KT
 from django.test import TestCase
 
+import thelabdb.fields
+
 from .models import Color, Product, ProductAttributes, Size, StoreID
 
 
 class PydanticFieldTest(TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.valid_attrs = ProductAttributes(
             uuid=UUID("d22f82ba-c94e-47ba-92cb-3e1eac6dd0de"),
             online=True,
@@ -33,21 +36,21 @@ class PydanticFieldTest(TestCase):
         )
         self.invalid_attrs_dict = self.invalid_attrs.model_dump(mode="json")
 
-    def test_insert(self):
+    def test_insert(self) -> None:
         """
         Pydantic model is serialized to JSON and stored in DB as a JSON field.
         """
-        Product.objects.create(attrs=self.valid_attrs)
+        Product._default_manager.create(attrs=self.valid_attrs)
         with connection.cursor() as cur:
             cur.execute("SELECT attrs FROM %s" % Product._meta.db_table)
             rows = [r[0] for r in cur.fetchall()]
         self.assertEqual(len(rows), 1)
         self.assertJSONEqual(rows[0], self.valid_attrs_dict)
 
-    def test_insert_and_select(self):
+    def test_insert_and_select(self) -> None:
         """Data round-trips through insert and select."""
-        Product.objects.create(attrs=self.valid_attrs)
-        p = Product.objects.get()
+        Product._default_manager.create(attrs=self.valid_attrs)
+        p = Product._default_manager.get()
         self.assertIsInstance(p, Product)
         self.assertIsInstance(p.attrs, ProductAttributes)
         self.assertEqual(p.attrs, self.valid_attrs)
@@ -59,34 +62,34 @@ class PydanticFieldTest(TestCase):
         # Test that datetimes work
         self.assertIsInstance(p.attrs.first_available_dt, datetime)
 
-    def test_update_and_select(self):
+    def test_update_and_select(self) -> None:
         """Data round-trips through update and select."""
-        Product.objects.create(attrs=self.valid_attrs)
+        Product._default_manager.create(attrs=self.valid_attrs)
 
         attrs2 = self.valid_attrs.model_copy(deep=True)
         attrs2.online = False
         attrs2.in_store_locations = []
-        Product.objects.update(attrs=attrs2)
+        Product._default_manager.update(attrs=attrs2)
 
-        found = Product.objects.get()
+        found = Product._default_manager.get()
         self.assertNotEqual(found.attrs, self.valid_attrs)
         self.assertEqual(found.attrs, attrs2)
 
-    def test_query_properties(self):
+    def test_query_properties(self) -> None:
         """Can use JSONField-style queries on model properties"""
-        Product.objects.create(attrs=self.valid_attrs)
+        Product._default_manager.create(attrs=self.valid_attrs)
 
-        objs = Product.objects.filter(attrs__color=Color.RED).all()
+        objs = Product._default_manager.filter(attrs__color=Color.RED).all()
         self.assertEqual(objs.count(), 1)
 
-        objs = Product.objects.filter(attrs__color=Color.BLUE).all()
+        objs = Product._default_manager.filter(attrs__color=Color.BLUE).all()
         self.assertEqual(objs.count(), 0)
 
-    def test_annotated_select(self):
+    def test_annotated_select(self) -> None:
         """Can use annotate() to extract properties from the field"""
-        Product.objects.create(attrs=self.valid_attrs)
+        Product._default_manager.create(attrs=self.valid_attrs)
         p = (
-            Product.objects.annotate(
+            Product._default_manager.annotate(
                 locations=KT("attrs__in_store_locations"),
                 price=KT("attrs__price"),
             )
@@ -95,63 +98,69 @@ class PydanticFieldTest(TestCase):
         )
         # TODO: This works, but the output is always just a string. Figure out
         # a better way to handle that.
-        self.assertJSONEqual(p["locations"], [123, 456])
-        self.assertEqual(p["price"], "123.45")
+        self.assertJSONEqual(p["locations"], [123, 456])  # type:ignore[index]
+        self.assertEqual(p["price"], "123.45")  # type:ignore[index]
 
-    def test_clean_valid(self):
+    def test_clean_valid(self) -> None:
         """Ensure Model.clean() passes when the model has valid data"""
         p = Product()
         p.attrs = self.valid_attrs
         p.full_clean()
 
-    def test_clean_invalid_properties(self):
+    def test_clean_invalid_properties(self) -> None:
         """Ensure Model.clean() errs when the model has invalid data"""
         p = Product()
         p.attrs = self.invalid_attrs
         with self.assertRaises(ValidationError):
             p.full_clean()
 
-    def test_clean_invalid_type(self):
+    def test_clean_invalid_type(self) -> None:
         """
         Ensure Model.clean() errs when the attrs is set to something other than
         the model type
         """
         p = Product()
-        p.attrs = 42
+        p.attrs = 42  # type:ignore[assignment]
         with self.assertRaises(ValidationError):
             p.full_clean()
 
-    def test_select_invalid(self):
+    def test_select_invalid(self) -> None:
         """Loading invalid data, by default, throws a ValidationError"""
-        Product.objects.create(attrs=self.invalid_attrs)
+        Product._default_manager.create(attrs=self.invalid_attrs)
         with self.assertRaises(ValidationError):
-            Product.objects.get()
+            Product._default_manager.get()
 
-    def test_select_invalid_coerced(self):
+    def test_select_invalid_coerced(self) -> None:
         """Allow massaging invalid data upon load (e.g. for data migrations)"""
         coerce_fn = Mock()
         coerce_fn.return_value = self.valid_attrs_dict
 
-        attrs_field = Product._meta.get_field("attrs")
+        attrs_field = cast(
+            thelabdb.fields.PydanticField[ProductAttributes],
+            Product._meta.get_field("attrs"),
+        )
         attrs_field.coerce_invalid_data = coerce_fn
 
-        Product.objects.create(attrs=self.invalid_attrs)
+        Product._default_manager.create(attrs=self.invalid_attrs)
         try:
-            p = Product.objects.get()
+            p = Product._default_manager.get()
             coerce_fn.assert_called_once_with(self.invalid_attrs_dict)
             self.assertIsInstance(p.attrs, ProductAttributes)
             self.assertEqual(p.attrs, self.valid_attrs)
         finally:
             attrs_field.coerce_invalid_data = None
 
-    def test_select_invalid_forced(self):
+    def test_select_invalid_forced(self) -> None:
         """Allows force loading invalid data"""
-        attrs_field = Product._meta.get_field("attrs")
+        attrs_field = cast(
+            thelabdb.fields.PydanticField[ProductAttributes],
+            Product._meta.get_field("attrs"),
+        )
         attrs_field.force_load_invalid_data = True
 
-        Product.objects.create(attrs=self.invalid_attrs)
+        Product._default_manager.create(attrs=self.invalid_attrs)
         try:
-            p = Product.objects.get()
+            p = Product._default_manager.get()
             self.assertIsInstance(p.attrs, ProductAttributes)
             # Have to compare the dumped versions, since the UUID and dates
             # won't be correctly instantiated. They'll just be strings.
